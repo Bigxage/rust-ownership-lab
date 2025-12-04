37ab37012c80f023eb181fb4f54bfc220c92c189
root <root@Baaye.localdomain>
2025-12-04 08:03:00 +0100

    day 09: implemented struct methods (verbs for the noun)

--- src/bin/day09_methods.rs
+++ src/bin/day09_methods.rs
@@ -4,12 +4,19 @@ fn main() {
         balance: u64,
         is_active: bool,
     }
+    impl SolanaUser {
+        fn introduce(&self){
+            println!("Hello, I am {}", self.username);
+            println!("I have {} SOL", self.balance);
+        }
+    }
     let user1 = SolanaUser {
         username: String::from("bigxage"),
         balance: 1000,
         is_active: true,
     };
     println!("user: {}", user1.username);
-    println!("balance: {} SOL", user1.balance);
+    println!("balance: {}", user1.balance);
     println!("active: {}", user1.is_active);
+    user1.introduce()
 }
