5a9c6d03b7266333bab4596eaf83e300747e0562
bigxage <haixage@gmail.com>
2025-12-07 14:04:53 +0100

    day 13/180: refactored to use constructor pattern

--- src/bin/day13_structs.rs
+++ src/bin/day13_structs.rs
@@ -4,14 +4,27 @@ struct LandPlot {
     is_sold: bool,
 }
 
+impl LandPlot {
+    fn new(size:u64, price: u64) -> LandPlot{
+        return LandPlot{
+            size: size,
+            price: price,
+            is_sold: false,
+        };
+    }
+
+    fn calculate_fee(&self) -> u64 {
+        return self.price * 5 / 100;
+    }
+}
+
 fn main() {
-    let lekki_plot = LandPlot {
-        size: 500,
-        price: 1000,
-        is_sold: false,
-    };
+    let lekki_plot = LandPlot::new(1000,50000);
 
     println!("Plot size: {} sqm", lekki_plot.size);
-    println!("Price: {} SOL", lekki_plot.price);
+    println!("Plot Price: {} SOL", lekki_plot.price);
     println!("Is it sold? {}", lekki_plot.is_sold);
+
+    let fee = lekki_plot.calculate_fee();
+    println!("Agency Fee (5%): {} SOL", fee);
 }
